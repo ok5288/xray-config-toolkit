18e113a6806d7059a2f710d1b9f47f0dc4eabdd3
ok5288 <130146065+ok5288@users.noreply.github.com>
2024-10-25 20:34:22 +0800

    Update worker.js

--- output/cloudflare/worker.js
+++ output/cloudflare/worker.js
@@ -168,19 +168,19 @@ async function handleUrlType(list) {
 
 const proxiesList = [
   {
-    name: 'wuqb2i4f',
+    name: 'Link',
     type: 'url',
-    data: 'https://raw.githubusercontent.com/wuqb2i4f/xray-config-toolkit/main/output/base64/mix'
+    data: 'https://raw.githubusercontent.com/ok5288/xray-config-toolkit/main/output/base64/mix'
   },
   {
-    name: 'wuqb2i4f-fragment',
+    name: 'Link-fragment',
     type: 'url',
-    data: 'https://raw.githubusercontent.com/wuqb2i4f/xray-config-toolkit/main/output/json/custom/mix-fragment.json'
+    data: 'https://raw.githubusercontent.com/ok5288/xray-config-toolkit/main/output/json/custom/mix-fragment.json'
   },
   {
-    name: 'wuqb2i4f-balancer',
+    name: 'Link-balancer',
     type: 'url',
-    data: 'https://raw.githubusercontent.com/wuqb2i4f/xray-config-toolkit/main/output/json/custom/mix-balancer.json'
+    data: 'https://raw.githubusercontent.com/ok5288/xray-config-toolkit/main/output/json/custom/mix-balancer.json'
   },
   {
     name: 'sample-list-uri',
